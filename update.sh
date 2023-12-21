03337437d245ab51b87ea018d5c24dc79599d055
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-12-21 09:30:36 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,5 @@
-#!/bin/sh
-
-# 检查当前的执行环境
-if [ -t 0 ]; then
-  # 终端执行环境，设置为0100755
-  chmod 755 "$0"
-else
-  # 非终端执行环境（如Git提交），设置为0100644
-  chmod 644 "$0"
-fi
+#!/usr/bin/env bash
+cd $(dirname "$0")
 
 echo "开始重新压缩deb"
 dpkg-scanpackages -m debs/rootless/ /dev/null > Packages
@@ -16,4 +8,4 @@ echo "完成压缩deb"
 echo "压缩Packages.bz2、Packages.xz"
 bzip2 -c Packages > Packages.bz2
 gzip -c Packages > Packages.xz
-echo "重新产生Packages.bz2、Packages.xz完成"
+echo "压缩完成"
