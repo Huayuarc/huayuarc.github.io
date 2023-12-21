3059279e2cbc428ca16cb6857b4eb640403fdf9c
wwg135 <13538342878@139.com>
2023-12-21 09:13:45 +0800

    更新文件

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "开始重新压缩deb"
-dpkg-scanpackages -m ./debs/rootless/ /dev/null >Packages
+dpkg-scanpackages -m debs/rootless/ /dev/null >Packages
 echo "完成压缩deb"
+
+echo "压缩Paackages.bz2、Packages.xz"
 bzip2 -c Packages > Packages.bz2
-gzip -c Packages > Packages.gz
+gzip -c Packages > Packages.xz
 echo "重新产生Paackages.bz2、Packages.xz完成"
