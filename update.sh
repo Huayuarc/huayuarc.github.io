24a591565de2dc32f32b850c5fa15634950fe46d
wwg135 <13538342878@139.com>
2023-12-25 13:47:19 +0800

    更新文件

--- update.sh
+++ update.sh
@@ -4,8 +4,7 @@ echo "开始压缩deb"
 dpkg-scanpackages -m . /dev/null > Packages
 echo "完成压缩deb"
 
-echo "压缩.bz2、.xz、.gz"
+echo "压缩.bz2、.xz"
 bzip2 -c Packages > Packages.bz2
 xz -c Packages > Packages.xz
-gzip -c Packages > Packages.gz
 echo "压缩完成"
