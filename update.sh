b2651eccb58c29ff9eb175fc0d5c01ea9bee143c
wwg135 <13538342878@139.com>
2023-12-25 09:34:35 +0800

    更新文件

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /var/mobile/github
+
+echo "开始压缩deb"
+dpkg-scanpackages -m . /dev/null > Packages
+echo "完成压缩deb"
+
+echo "压缩.bz2、.xz、.gz"
+bzip2 -c Packages > Packages.bz2
+xz -c Packages > Packages.xz
+gzip -c Packages > Packages.gz
+echo "压缩完成"
