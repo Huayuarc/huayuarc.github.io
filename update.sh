c06bc12035b5995457c114b8e7c03d361629ab18
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-12-26 15:16:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,15 @@ echo "开始压缩deb"
 dpkg-scanpackages -m . /dev/null > Packages
 echo "压缩deb完成"
 
-echo "开始压缩.bz2、.xz"
+echo "开始压缩.bz2、.xz、.gz、.lzma、.zst"
 bzip2 -c Packages > Packages.bz2
 xz -c Packages > Packages.xz
+gzip -c Packages > Packages.gz
+lzma -c Packages > Packages.lzma
+zstd -c Packages > Packages.zst
 echo "压缩完成"
+
+echo "git到远程仓库github"
+git add .
+git commit -m "更新文件"
+git push
