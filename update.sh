aceb415ab034b1a777dba702f9a7f45656ae0377
wwg135 <13538342878@139.com>
2024-01-26 13:16:13 +0800

    更新文件

--- update.sh
+++ update.sh
@@ -4,12 +4,9 @@ echo "开始压缩deb"
 dpkg-scanpackages -m . /dev/null > Packages
 echo "压缩deb完成"
 
-echo "开始压缩.bz2、.xz、.gz、.lzma、.zst"
+echo "开始压缩.bz2、.xz"
 bzip2 -c Packages > Packages.bz2
 xz -c Packages > Packages.xz
-gzip -c Packages > Packages.gz
-lzma -c Packages > Packages.lzma
-zstd -c Packages > Packages.zst
 echo "压缩完成"
 
 echo "推送到远程仓库github"
