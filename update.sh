edc7ef3e4e1c3fcfab85355cd3bd3d2270968c1e
wwg135 <13538342878@139.com>
2024-03-10 05:53:11 +0800

    更新文件

--- update.sh
+++ update.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
-echo "开始压缩deb"
 dpkg-scanpackages -m . /dev/null > Packages
-echo "压缩deb完成"
-
-echo "开始压缩.bz2、.xz"
 bzip2 -c Packages > Packages.bz2
 xz -c Packages > Packages.xz
-echo "压缩完成"
 
-echo "推送到远程仓库github"
 git add .
 git commit -m "更新文件"
 git push
-echo "更新完成"
